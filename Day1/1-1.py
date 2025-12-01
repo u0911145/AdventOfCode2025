eafc96b4e5633a634722d494f1b6f21360463394
u0911145 <u0911145@utah.edu>
2025-12-01 16:02:12 -0700

    Day1

---
+++ Day1/1-1.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+
+filename = sys.argv[1] if len(sys.argv) > 1 else exit('Please provide an input file.')
+data = np.loadtxt(filename, dtype=str)
+
+count = 50
+pswd = 0
+for item in data:
+    dir = -1 if item[0] == 'L' else 1
+    num = int(item[1:])
+    count = (count + dir * num) % 100
+    if count == 0:
+        pswd += 1
+print(pswd)
