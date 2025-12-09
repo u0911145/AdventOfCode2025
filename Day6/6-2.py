bc9036beb221649420b8a0e6b24670389eb94f4b
Kylen Xiao <ZeldaSpider599@gmail.com>
2025-12-08 23:49:04 -0700

    Update 6-2.py

--- Day6/6-2.py
+++ Day6/6-2.py
@@ -2,18 +2,11 @@ import sys
 import numpy as np
 
 filename = sys.argv[1] if len(sys.argv) > 1 else exit('Please provide an input file.')
-data = np.array([[r[0], ''.join(r[1:])] for r in np.roll(np.genfromtxt(filename, dtype=str, delimiter=1).transpose(), shift=1, axis=1)])
-data = data[:, 1].reshape((-1, 3))
-print(data)
-# op = ""
-# count = 0
-# temp = 0
-# for line in data:
-#     if (line[1].isspace()):
-#         count += temp
-#         temp = 0
-#     else:
-#         if not line[0].isspace():
-#             op = line[0]
-#         num = int(line[1])
-#         temp = num if temp == 0 else temp * num if op == '*' else temp + num
+data = np.append(np.array([[r[0], ''.join(r[1:])] for r in np.roll(np.genfromtxt(filename, dtype=str, delimiter=1).transpose(), shift=1, axis=1)]), [[' ', ' ']], axis=0)
+opindices = np.concatenate(np.where(np.logical_or(~np.char.isspace(data[:, 0]), np.char.isspace(data[:, 1])))).reshape((-1, 2))
+
+count = 0
+for set in opindices:
+    nums = np.array([int(x) for x in data[set[0]:set[1], 1]])
+    count += nums.sum() if data[set[0], 0] == '+' else nums.prod()
+print(count)
